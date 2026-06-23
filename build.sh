feb09d3d4ba4c881f01eb941da32643b5b4f1988
Kevin Bueno <KevzBueno101@users.noreply.github.com>
2026-06-23 13:51:37 +0800

    update: superadmin cred

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+if not User.objects.filter(username='kevin').exists():
+    User.objects.create_superuser('kevin', 'kevinbueno360@gmail.com', 'kevz@rents')
     print('Superuser created')
 else:
     print('Superuser already exists')
